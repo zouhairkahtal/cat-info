6e2418ed6bef4223d9e39a96a815973047f79198
zouhair <mizouhair4@gmail.com>
2025-11-18 21:57:41 +0100

    .

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -3,14 +3,9 @@ import CatImage from "../assets/HomePageImage/HomePageCat.png";
 
 import { Nav } from "../components/nav";
 import { NavLink } from "react-router-dom";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 
 export function HomePage() {
-
-
-
-
-
   return (
     <>
       {/* <button className="bg-red-300">
@@ -38,37 +33,40 @@ export function HomePage() {
                 many breeds, each with unique traits
               </p>
               <NavLink to={"/DogPage"}>
-              <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="py-2 px-5 bg-orange-400 rounded-full font-medium text-orange-900 ease-out duration-300 border border-orange-400  hover:bg-orange-900 hover:text-orange-400 hover:scale-110 ">
-                
-                Read More
-              
-              </motion.button>
-              </NavLink>  
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="py-2 px-5 bg-orange-400 rounded-full font-medium text-orange-900 ease-out duration-300 border border-orange-400  hover:bg-orange-900 hover:text-orange-400 hover:scale-110 "
+                >
+                  Read More
+                </motion.button>
+              </NavLink>
 
               <div className="hidden max-[650px]:flex  h-36 w-full items-center justify-center gap-5">
-               
-               <NavLink to={"/DogInfo"}>
+                <NavLink to={"/DogInfo"}>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="border border-orange-400 bg-orange-900 text-orange-400 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 "
+                  >
+                    Info
+                  </motion.button>
+                </NavLink>
 
-                <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="border border-orange-400 bg-orange-900 text-orange-400 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 ">
-                  Info
-                </motion.button>
-               </NavLink>
-
-               <NavLink to={"/DogGallery"}>
-
-                <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="border border-orange-400 bg-orange-900  text-orange-400 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 ">
-                  Gallery
-                </motion.button>
-               </NavLink>
+                <NavLink to={"/DogGallery"}>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="border border-orange-400 bg-orange-900  text-orange-400 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 "
+                  >
+                    Gallery
+                  </motion.button>
+                </NavLink>
               </div>
             </div>
           </div>
         </div>
         <div className="h-full w-6 bg-gradient-to-r max-[650px]:bg-gradient-to-b from-orange-900 to-orange-400 max-[650px]:w-full "></div>
-
 
         <div className="w-6/12 h-full bg-orange-400 flex items-center justify-center pt-20 max-[650px]:pt-2 max-[650px]:w-full">
           <div className=" max-w-[600px] w-8/12 h-[700px] max-[650px]:w-full max-[650px]:px-4  ">
@@ -87,28 +85,35 @@ export function HomePage() {
                 curiosity, elegance, and calm nature. They can be both playful
                 and mysterious, bringing warmth and charm to any home
               </p>
-        <NavLink to={"/CatPage"}>
+              <NavLink to={"/CatPage"}>
+                <motion.button
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="py-2 px-5 bg-orange-900 rounded-full font-medium text-orange-400 ease-out duration-300 border border-orange-900  hover:bg-orange-400 hover:text-orange-900 hover:scale-110 "
+                >
+                  Read More
+                </motion.button>
+              </NavLink>
 
-              <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="py-2 px-5 bg-orange-900 rounded-full font-medium text-orange-400 ease-out duration-300 border border-orange-900  hover:bg-orange-400 hover:text-orange-900 hover:scale-110 ">
-                Read More
-              </motion.button>
-        </NavLink>
-              
               <div className="hidden max-[650px]:flex  h-36 w-full items-center justify-center gap-5 ">
-
-<NavLink to={"/CatInfo"}>
-                <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="border border-orange-900 bg-orange-400 text-orange-900 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 ">
-                  Info
-                </motion.button>
-</NavLink>
-          <NavLink to={"/Gallery"}>
-                <motion.button   whileHover={{ scale: 1.1 }}
-  whileTap={{ scale: 0.95 }} className="border border-orange-900 bg-orange-400  text-orange-900 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 ">
-                  Gallery
-                </motion.button>
-            </NavLink>      
+                <NavLink to={"/CatInfo"}>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="border border-orange-900 bg-orange-400 text-orange-900 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 "
+                  >
+                    Info
+                  </motion.button>
+                </NavLink>
+                <NavLink to={"/Gallery"}>
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="border border-orange-900 bg-orange-400  text-orange-900 py-2 px-8 rounded-full text-lg font-semibold  hover:scale-105 ease-out duration-300 "
+                  >
+                    Gallery
+                  </motion.button>
+                </NavLink>
               </div>
             </div>
           </div>
